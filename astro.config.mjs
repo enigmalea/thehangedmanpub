89188b8ee414b2930e267debc2eba0a95bdab0f2
enigmalea <enigmalea@gmail.com>
2024-02-16 17:43:22 -0500

    added pages from old site.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
+import icon from "astro-icon";
 import metaTags from "astro-meta-tags";
 import sitemap from "@astrojs/sitemap";
 import starlight from "@astrojs/starlight";
@@ -11,6 +12,7 @@ export default defineConfig({
   site: "https://thehangedmanpub.netlify.app",
   extends: "astro/tsconfigs/strictest",
   integrations: [
+    icon(),
     tailwind(),
     sitemap(),
     metaTags(),
@@ -18,6 +20,43 @@ export default defineConfig({
       themes: ["nord"],
     }),
     starlight({
+      head: [
+        {
+          tag: "meta",
+          attrs: {
+            property: "theme-color",
+            content: "#2F3136",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            property: "og:image",
+            content: "./banner.png",
+          },
+        },
+				{
+          tag: "meta",
+          attrs: {
+            property: "og:image:type",
+            content: "image/png",
+          },
+        },
+				{
+          tag: "meta",
+          attrs: {
+            property: "og:image:height",
+            content: "499",
+          },
+        },
+				{
+          tag: "meta",
+          attrs: {
+            property: "og:image:width",
+            content: "1072",
+          },
+        },
+      ],
       title: "The Hanged Man",
       favicon: "/favicon.ico",
       logo: {
@@ -33,17 +72,72 @@ export default defineConfig({
         discord: "https://discord.gg/9RERC6R",
       },
       sidebar: [
+        { label: "Home", link: "/" },
+        { label: "Code of Conduct", link: "/code" },
+        { label: "Leadership Team", link: "/leadership" },
         {
-          label: "Guides",
+          label: "Discord Server Resources",
           items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", link: "/guides/example/" },
+            { label: "Links", link: "/discord/links" },
+            {
+              label: "Fic Club",
+              items: [
+                { label: "Introduction", link: "/discord/club" },
+                { label: "Suggest a Fic", link: "/discord/club/suggest" },
+                { label: "Fic Club Database", link: "/discord/club/database" },
+              ],
+            },
+            {
+              label: "Question of the Day",
+              items: [
+                { label: "Introduction", link: "/discord/qotd" },
+                { label: "Submit a Question", link: "/discord/qotd/question" },
+                {
+                  label: "Submit a Monthly Prompt",
+                  link: "/discord/qotd/prompt",
+                },
+                { label: "Submit a Picrew", link: "/discord/qotd/picrew" },
+                { label: "Archives", link: "/discord/qotd/archives" },
+              ],
+            },
           ],
         },
         {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
+          label: "Communities",
+          items: [
+            {
+              label: "Discord",
+              link: "https://discord.gg/9RERC6R",
+              attrs: { target: "_blank" },
+            },
+            {
+              label: "Pillowfort",
+              link: "https://www.pillowfort.social/community/The%20Hanged%20Man",
+              attrs: { target: "_blank" },
+            },
+            {
+              label: "Dreamwidth",
+              link: "https://hangedmanpub.dreamwidth.org/",
+              attrs: { target: "_blank" },
+            },
+          ],
         },
+        {
+          label: "Events",
+          items: [
+            {
+              label: "Arlathan eXchange",
+              link: "https://arlathanxchange.neocities.org",
+              attrs: { target: "_blank" },
+            },
+            {
+              label: "DA Polyshipping",
+              link: "https://dapolyshipping.neocities.org",
+              attrs: { target: "_blank" },
+            },
+          ],
+        },
+        { label: "Credits", link: "/credits" },
       ],
       plugins: [starlightLinksValidator()],
     }),
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,37 @@
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
+
 import { docsSchema } from "@astrojs/starlight/schema";
+
+const icon = [
+  "fa6-solid:link",
+  "simple-icons:firefish",
+  "simple-icons:archiveofourown",
+  "simple-icons:mastodon",
+  "simple-icons:tumblr",
+  "simple-icons:twitter",
+  "simple-icons:instagram",
+] as const;
+
+const communities = ["discord", "dreamwidth", "pillowfort"] as const;
+
+const events = ["arlathan", "da poly"] as const;
+
+const modCollection = defineCollection({
+  type: "data", // v2.5.0 and later
+  schema: z.object({
+    name: z.string(),
+    avatar: z.string(),
+    links: z
+      .object({
+        icon: z.enum(icon),
+        url: z.string().url(),
+      })
+      .array(),
+    mod_duties: z.enum([...communities, ...events]).array(),
+  }),
+});
 
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
+  mods: modCollection,
 };
