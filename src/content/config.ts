f877db45ed7e90c3fa241a86cd9a1c77b310b01e
enigmalea <admin@enigmalea.quest>
2026-02-08 10:47:54 -0500

    update Leadership Team with fujocoded plugin.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,9 @@
 import { defineCollection, z } from "astro:content";
 
+import { SocialLinks } from "@fujocoded/zod-transform-socials";
 import { autoSidebarLoader } from "starlight-auto-sidebar/loader";
 import { autoSidebarSchema } from "starlight-auto-sidebar/schema";
 import { docsSchema } from "@astrojs/starlight/schema";
-
-const icon = [
-  "fa6-solid:link",
-  "simple-icons:firefish",
-  "simple-icons:archiveofourown",
-  "simple-icons:mastodon",
-  "simple-icons:tumblr",
-  "simple-icons:twitter",
-  "simple-icons:instagram",
-] as const;
 
 const communities = ["discord", "dreamwidth", "pillowfort"] as const;
 
@@ -24,12 +15,7 @@ const modCollection = defineCollection({
     z.object({
       name: z.string(),
       avatar: tools.image(),
-      links: z
-        .object({
-          icon: z.enum(icon),
-          url: z.string().url(),
-        })
-        .array(),
+      links: SocialLinks,
       mod_duties: z.enum([...communities, ...events]).array(),
     }),
 });
